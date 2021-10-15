0a9f6dae6228ad013d957eac012092ff51f94070
Cosmin Deaconu <cozzyd@gmail.com>
2021-10-15 14:17:45 -0500

    simple deployment script

---
+++ deploy.sh
@@ -0,0 +1,22 @@
+#! /bin/sh 
+
+if [ ! -d dist ] ; then 
+  yarn build 
+fi 
+
+DEST=${1-/var/lib/grafana/plugins} 
+NAME=${2-interposcatter} 
+
+if [ -e "${DEST}/${NAME}" ] ; then
+  read -r -p "Nuke ${DEST}/${NAME}? (y/n)" response 
+  case "$response" in [yY][eE][sS]|[yY])
+    rm -rf $DEST/$NAME
+    ;;
+  *) 
+    exit 1
+    ;;
+  esac
+fi
+
+
+cp -r dist/ ${DEST}/${NAME} 
