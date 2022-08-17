869bb274433800a4f6723e1eb7da8e539d3cd5da
Cosmin Deaconu <cozzyd@gmail.com>
2022-08-17 13:46:14 -0500

    missing space

--- deploy.sh
+++ deploy.sh
@@ -8,7 +8,7 @@ DEST=${1-/var/lib/grafana/plugins}
 NAME=${2-interposcatter} 
 
 if [ -e "${DEST}/${NAME}" ] ; then
-  read -r -p "Nuke ${DEST}/${NAME}? (y/n)" response 
+  read -r -p "Nuke ${DEST}/${NAME}? (y/n) " response 
   case "$response" in [yY][eE][sS]|[yY])
     rm -rf $DEST/$NAME
     ;;
