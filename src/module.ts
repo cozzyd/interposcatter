7b41c88a5d91876c67cf595882081fd9bdc25dab
Cosmin Deaconu <cozzyd@gmail.com>
2022-08-17 15:59:29 -0500

    add selector

--- src/module.ts
+++ src/module.ts
@@ -1,5 +1,29 @@
+//
+// This file is part of interposcatter
+//
+//(C) 2021 Cosmin Deaconu <cozzyd@kicp.uchicago.edu>
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy of
+// this software and associated documentation files (the "Software"), to deal in
+// the Software without restriction, including without limitation the rights to
+// use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+// of the Software, and to permit persons to whom the Software is furnished to do
+// so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE
+
 import { PanelPlugin } from '@grafana/data';
 import { InterpoScatterOptions } from './types';
+import { SeriesSelector } from './SeriesSelector';
 import { InterpoScatterPanel } from './InterpoScatterPanel';
 
 export const plugin = new PanelPlugin<InterpoScatterOptions>(InterpoScatterPanel).setPanelOptions(builder => {
@@ -25,17 +49,18 @@ export const plugin = new PanelPlugin<InterpoScatterOptions>(InterpoScatterPanel
         ],
       },
     })
-    .addTextInput({
+    .addCustomEditor({
       path: 'xName',
-      defaultValue: '',
-      name: 'x series (leave blank for first available, or use @i@ for ith, 0-indexed)',
+      id: 'xName',
+      editor: SeriesSelector,
+      name: 'x series',
     })
-    .addTextInput({
+    .addCustomEditor({
       path: 'yName',
-      defaultValue: '',
-      name: 'y series (leave blank for first available, or use @i@ for ith, 0-indexed)',
+      id: 'yName',
+      editor: SeriesSelector,
+      name: 'y series',
     })
-
     .addRadio({
       path: 'edgeBehavior',
       defaultValue: 'ignore',
